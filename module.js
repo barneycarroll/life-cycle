3fa04b606ad28ee9dba5c930d03a14871c1732d7
Barney Carroll <barney.carroll@gmail.com>
2021-03-18 20:17:13 +0000

    Expose element class as an export

---
+++ module.js
@@ -0,0 +1,31 @@
+export default class LifeCycle extends HTMLElement {
+  connected(){}
+  connectedCallback(){
+    this.style.display = 'contents'
+    
+    this.connected.apply(this, arguments)
+  }
+
+  disconnected(){}
+  disconnectedCallback(){
+    this.disconnected.apply(this, arguments)
+  }
+
+  adopted(){}
+  adoptedCallback(){
+    this.adopted.apply(this, arguments)
+  }
+  
+  attributeChanged(){}
+  attributeChangedCallback(){
+    this.attributeChanged.apply(this, arguments)
+  }
+  
+  get updated(callback){
+    updated.call(this)
+  }
+  
+  static get observedAttributes(){
+    return ['attribute']
+  }
+}
